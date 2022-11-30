164e3571780c91bcce95b3b9eb21bb60120b9cda
Joshua <joshsmayhew@gmail.com>
2022-11-30 10:56:12 +0700

    commit tree 1e8f3d1749f7ef1b337c12ec9862ca6aa9097cb0

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def valid?
+    unless @side_1.positive? && @side_2.positive? && @side_3.positive? &&
+           @side_1 + @side_2 > @side_3 && @side_1 + @side_3 > @side_2 && @side_2 + @side_3 > @side_1
+
+      raise TriangleError
+    end
+  end
+
+  def kind
+    valid?
+
+    if @side_1 == @side_2 && @side_2 == @side_3
+      :equilateral
+    elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      'you must give the get_married method an argument of an instance of the person class!'
+    end
+  end
 end
